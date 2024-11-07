4057d29b93a422a41101e44f0cd153eb64e271b9
Dwi Kautsar <raysahdwi12@gmail.com>
2024-11-07 15:05:33 +0700

    test

---
+++ calculator.py
@@ -0,0 +1,6 @@
+a = 10
+b = 20
+def tambah(a,b):
+    print("Hasil nya adalah", a + b)
+    
+tambah(1,2)
---
+++ main.py
@@ -0,0 +1,7 @@
+print("Selamat Siang")
+x = 0
+while True:
+    x += 1
+    if x > 50:
+        break
+    print(x)
